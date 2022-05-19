5fe76492a6dd4e710a13e0b839c76798bb469e44
DarkB0B <miecze123@onet.pl>
2022-05-19 22:49:18 +0200

    tests init

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeTimePeriod;
+using System;
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
